21826a55538c7ac119b4ab59751e44ac6cc3f5de
Bai Jaitrong <bjaitrong@comcast.net>
2022-09-24 14:20:51 -0600

    WIP: Add UI logic displayCost function and CSS styles rules for display.

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,38 @@ Pizza.prototype.cost = function() {
 }
 
 //User Interface Logic
+function displayCost(pizzaOrder) {
 
-function handleForm (event) {
+  const form = document.querySelector("form#pizzaOrder");
+  form.remove();
+
+  pizzaOrder.toppings.forEach(function(e) {
+    const p = document.createElement("p");
+    p.append(e.value);
+    document.body.append(p);
+  });
+
+  const p = document.createElement("p");
+  p.append(pizzaOrder.cost());
+  document.body.append(p);
+
+}
+
+
+
+function handleForm(event) {
   event.preventDefault;
   
   const toppings = document.querySelectorAll("input[name=toppings]:checked");
   const pizzaSize = document.querySelector("input[name='size']:checked").value;
 
   const toppingsArray = Array.from(toppings);
+
+  let pizzaOrder = new Pizza(toppingsArray,pizzaSize);
   
+  displayCost(pizzaOrder);
+  
+
 }
 
 window.addEventListener("load", function() {
